cdd5b245c8b84edf1124e9fa8e2fe39cd5db6cc3
ryanaPc <aryansehgal3212@gmail.com>
2026-02-12 23:57:19 +0530

    basic web server template to refer to

---
+++ projects/web_server/main.go
@@ -0,0 +1,34 @@
+package main 
+
+import (
+	"fmt"     
+	"log"      
+	"log/slog" // Structured logging (key-value style logs, newer than log)
+	"net/http" 
+)
+
+func main() {
+	http.HandleFunc("/", handleHello)
+	// nil means "use the default ServeMux" (the default router)
+	// log.Fatal will log an error AND stop the program if server fails
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+// w = ResponseWriter (used to send data back to client)
+// r = Request (contains request info like headers, body, etc.)
+// We ignore r here using _ because we don’t need it
+func handleHello(w http.ResponseWriter, _ *http.Request) {
+
+	// Write sends bytes as HTTP response to the client
+	// []byte converts string to byte slice (Write needs bytes)
+	// wc = number of bytes written
+	// err = error if something went wrong
+	wc, err := w.Write([]byte("Hello World\n"))
+
+	// Always check errors when writing responses
+	if err != nil {
+		slog.Error("Error writing response", "err", err)
+		return 
+	}
+	fmt.Printf("%d bytes written\n", wc)
+}
